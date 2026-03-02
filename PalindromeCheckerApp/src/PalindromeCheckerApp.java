cd766145123e374fb30be1ae680e2be17b8be73e
pg3566-oss <pg3566@srmist.edu.in>
2026-03-02 10:18:18 +0530

    pg3566-oss UC5

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,34 +1,36 @@
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
 
     /**
-     * Application entry point for UC4.
-     * Character Array Based Palindrome Check
+     * Application entry point for UC5.
+     * Stack-Based Palindrome Checker
      */
 
     public static void main(String[] args) {
 
-        // Step 1: Store the original string
+        // Step 1: Store input string
         String input = "madam";
 
-        // Step 2: Convert string to character array
-        char[] charArray = input.toCharArray();
+        // Step 2: Create a stack
+        Stack<Character> stack = new Stack<>();
 
-        // Step 3: Initialize two pointers
-        int start = 0;
-        int end = charArray.length - 1;
+        // Step 3: Push all characters into stack
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
 
         boolean isPalindrome = true;
 
-        // Step 4: Two-pointer comparison
-        while (start < end) {
+        // Step 4: Pop from stack and compare with original string
+        for (int i = 0; i < input.length(); i++) {
 
-            if (charArray[start] != charArray[end]) {
+            char poppedChar = stack.pop();
+
+            if (input.charAt(i) != poppedChar) {
                 isPalindrome = false;
                 break;
             }
-
-            start++;
-            end--;
         }
 
         // Step 5: Display result

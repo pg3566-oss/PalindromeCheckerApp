a66d0b7a60d71c5aff8a5b6db7ecb770e04ab712
pg3566-oss <pg3566@srmist.edu.in>
2026-03-02 10:09:30 +0530

    pg3566-oss UC3

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,30 +1,25 @@
 public class PalindromeCheckerApp {
 
     /**
-     * Application entry point for UC2.
-     * Hardcoded Palindrome Validation
+     * Application entry point for UC3.
+     * Palindrome Check Using String Reverse
      */
 
     public static void main(String[] args) {
 
-        // Step 1: Store a predefined (hardcoded) string
+        // Step 1: Store original string
         String input = "madam";
 
-        // Step 2: Assume it is a palindrome initially
-        boolean isPalindrome = true;
+        // Step 2: Create an empty string to store reversed value
+        String reversed = "";
 
-        // Step 3: Loop only till half of the string length
-        for (int i = 0; i < input.length() / 2; i++) {
-
-            // Step 4: Compare characters from both ends
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+        // Step 3: Reverse string using loop
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
         }
 
-        // Step 5: Display the result
-        if (isPalindrome) {
+        // Step 4: Compare original and reversed string
+        if (input.equals(reversed)) {
             System.out.println(input + " is a Palindrome");
         } else {
             System.out.println(input + " is NOT a Palindrome");
